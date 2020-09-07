48d1ae68d7316956c1348f8a598c5fcc1e7abc90
hyunduk-jo <69310028+hyunduk-jo@users.noreply.github.com>
2020-09-07 17:33:37 +0900

    download babel, express, nodemon

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hi")
+import express from "express";
+const app = express()
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`✅Listening on: http://localhost:${PORT}`)
+}
+
+const handleHome = (req, res, next) => {
+    res.send("Hello from home~!")
+}
+
+const handleProfile = (req, res) => {
+    res.send("You are in my Profile!!")
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
