968e68a786c9e4ab7569b0bb98ce1172e13a2658
hyunduk-jo <69310028+hyunduk-jo@users.noreply.github.com>
2020-09-10 16:06:25 +0900

    Add post

--- routers/controllers/userController.js
+++ routers/controllers/userController.js
@@ -1,10 +1,21 @@
+import routes from "../../routes";
+
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 };
 
 export const postJoin = (req, res) => {
-    console.log(req.body);
-    res.render("join", { pageTitle: "Join" });
+    const {
+        body: { name, email, password, password2 }
+    } = req;
+    if (password !== password2) {
+        res.status(400);
+        res.render("join", { pageTitle: "Join" });
+    } else {
+        // To Do: Register User
+        // To Do: Log user in
+        res.redirect(routes.home);
+    }
 };
 
 /*export const postJoin = (req, res) => {
@@ -12,9 +23,20 @@ export const postJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 }*/
 
+export const getLogin = (req, res) => {
+    res.render("login", { pageTitle: "Login" });
+}
 
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
+
+
+export const logout = (req, res) => {
+    // To Do: process log out
+    res.redirect(routes.home);
+}
+
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
--- routers/controllers/videoController.js
+++ routers/controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videosDB } from "../../db";
+import routes from "../../routes"
 
 export const home = (req, res) => {
     res.render("home", { pageTitle: "Home", videosDB });
@@ -7,8 +8,20 @@ export const search = (req, res) => {
     const { query: { term: searchingBy } } = req;
     res.render("search", { pageTitle: "Search", searchingBy, videosDB });
 }
-export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
-export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const getUpload = (req, res) => {
+    res.render("upload", { pageTitle: "Upload" });
+}
+
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description }
+    } = req;
+    // To Do: Upload and save video
+    res.redirect(routes.videoDetail(11111))
+}
+
+
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
